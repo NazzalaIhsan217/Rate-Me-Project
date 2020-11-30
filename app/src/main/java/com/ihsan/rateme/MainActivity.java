b54d9c1ade95d887eeb604125632f9a382fc1a61
nazzalaihsan <nazzalaihsan217@gmail.com>
2020-11-30 12:20:18 +0700

    Rate Me Project - Nazzala Ihsan N

---
+++ app/src/main/java/com/ihsan/rateme/MainActivity.java
@@ -0,0 +1,29 @@
+package com.ihsan.rateme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void rateMe(View v) {
+        try {
+            startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("market://details?id=" + getPackageName())));
+        } catch (ActivityNotFoundException e) {
+            startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://www.youtube.com/channel/UCKqPgWB-SdsUca54FgfX22A" + getPackageName())));
+        }
+    }
+
+}
